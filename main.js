166025ca3eee1a7c341a8cd855975d02ea12ca4b
Vermilion15 <seigneur.noir15@gmail.com>
2023-11-29 00:37:56 +0700

    change logical operator

--- main.js
+++ main.js
@@ -7,9 +7,9 @@ function submitForm(e){
     const subject = document.getElementById("inpt-subject");
     const message = document.getElementById("inpt-message");
 
-    if(!name.value && !email.value && !number.value && !subject.value && !message.value){
+    if(!name.value || !email.value || !number.value || !subject.value || !message.value){
         alert("Please fulfilled the field");
-    }else {
+    }else{
         alert("Your Message has  been SuccessFully Send");
         name.value = "";
         email.value = "";
